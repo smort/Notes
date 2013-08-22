1f5cec0325f3f3474e047af56d32abbc1e1c9d66
smort <samaccounts@fake.fake>
2013-08-21 22:47:21 -0600

    build changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,49 +2,48 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg : grunt.file.readJSON('package.json'),
 
-		concat : {
-			options : {
-				// define a string to put between each file in the concatenated output
-				separator : ';'
-			},
-			dist : {
-				// the files to concatenate
-				src : ['scripts/**/*.js'],
-				// the location of the resulting JS file
-				dest : 'dist/<%= pkg.name %>.js'
-			}
-		},
-
-		uglify : {
-			options : {
-				// the banner is inserted at the top of the output
-				banner : '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-			},
-			dist : {
-				files : {
-					'dist/<%= pkg.name %>.min.js' : ['<%= concat.dist.dest %>']
-				}
-			}
-		},
-
 		jshint : {
 			// define the files to lint
 			files : ['scripts/src/**/*.js'],
 			// configure JSHint (documented at http://www.jshint.com/docs/)
 			options : {
-				bitwise: true,
-				curly: true,
-				eqeqeq: true,
-				latedef: true,
-				undef: true,
-				unused: true,
+				bitwise : true,
+				curly : true,
+				eqeqeq : true,
+				latedef : true,
+				undef : true,
+				//unused: true,
 			},
-			globals: {
-				module: true,
-				console: true,
-				jQuery: true,
-				angular: true
+			globals : {
+				module : true,
+				console : true,
+				jQuery : true,
+				angular : true
 			}
+		},
+
+		copy : {
+			release : {
+				files : [{
+					src : 'index.html',
+					dest : 'release/'
+				}, 
+				{
+					src : 'partials/**',
+					dest : 'release/'
+				}]
+			}
+		},
+
+		useminPrepare : {
+			html : 'index.html',
+			options: {
+				dest: 'release/'
+			}
+		},
+
+		usemin : {
+			html : 'release/index.html'
 		},
 
 		watch : {
@@ -57,12 +56,14 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-usemin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-preprocess');
 
 	// this would be run by typing "grunt test" on the command line
 	grunt.registerTask('test', ['jshint']);
 	// the default task can be run just by typing "grunt" on the command line
 	grunt.registerTask('default', ['jshint']);
-	grunt.registerTask('release', ['jshint', 'concat', 'uglify']);
+	//grunt.registerTask('release', ['jshint', 'concat', 'uglify']);
+	grunt.registerTask('release', ['jshint', 'copy:release', 'useminPrepare', 'concat', 'uglify', 'usemin']);
 };
